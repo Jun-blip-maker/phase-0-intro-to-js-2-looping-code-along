a90d0690b6c571dba994691798e2597f9e3aa8a3
khadijun@DESKTOP <kardikhadija1@gmail.com>
2025-03-08 00:50:42 +0300

    phase-0-intro-to-js-2-looping-code-along

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+function writeCards(names = ["Guadalupe", "Ollie", "Aki"], event = "surprise") {
+  let messages = [];
+  let i = 0;
+  while (i < names.length) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    i++;
+  }
+  return messages;
+}
+// let callCount = (number)  {
+// for (let i = 0; i < 11; i++) {
+//   console.log(number[i]);
+// }
+// }
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
